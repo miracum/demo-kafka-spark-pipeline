ece0cbafb128dc0ca528a58b116d6e6ccef91a3a
Lorenz Kapsner <lorenz.kapsner@uk-erlangen.de>
2022-05-12 12:54:06 +0200

    added spark cluster

---
+++ spark/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# https://www.kdnuggets.com/2020/07/apache-spark-cluster-docker.html
+
+docker build -f Dockerfile.cluster-base -t cluster-base .
+docker build -f Dockerfile.spark-base -t spark-base .
+docker build -f Dockerfile.spark-master -t spark-master .
+docker build -f Dockerfile.spark-worker -t spark-worker .
